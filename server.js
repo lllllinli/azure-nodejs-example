068c7cd2c2ca5b0743d5dc9e7f9be70e9dd6adb3
standleylin <standleylin@gmail.com>
2016-03-21 11:20:30 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-var http = require('http');
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello from Azure running node version: ' + process.version + '</br>');
-}).listen(process.env.PORT || 3000);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+
+  console.log('port:',port);
+  res.send('<h1>Hello PORT:</h1>');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!');
+});
