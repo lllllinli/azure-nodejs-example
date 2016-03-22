9ae7e45343a5351a39bd3c6e446c1eb684e79589
standleylin <standleylin@gmail.com>
2016-03-22 12:18:30 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('You have reached the default node.js application at index.js! [defaultdocument sample]');
-}).listen(process.env.PORT);  
+// var http = require('http');
+//
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end('You have reached the default node.js application at index.js! [defaultdocument sample]');
+// }).listen(process.env.PORT);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
+ 
