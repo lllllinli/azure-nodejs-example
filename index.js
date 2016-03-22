5741b2f820a126bdac0ec19f734b874046f09eed
standleylin <standleylin@gmail.com>
2016-03-22 13:47:35 +0800

    update

--- index.js
+++ index.js
@@ -4,10 +4,18 @@
 //     res.writeHead(200, {'Content-Type': 'text/html'});
 //     res.end('You have reached the default node.js application at index.js! [defaultdocument sample]');
 // }).listen(process.env.PORT);
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
- 
+// var http = require('http')
+// var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(port);
+
+var koa = require('koa');
+var app = koa();
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(process.env.PORT || 3000);
